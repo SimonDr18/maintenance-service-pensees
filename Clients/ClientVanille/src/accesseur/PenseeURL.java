593a4e61b6f6e17b96c60967522cc0166f743afa
Simon <simon.drieux@orange.fr>
2019-02-15 13:45:41 -0500

    [MOD] Fix chemin URL pensee

--- Clients/ClientVanille/src/accesseur/PenseeURL.java
+++ Clients/ClientVanille/src/accesseur/PenseeURL.java
@@ -1,6 +1,6 @@
 package accesseur;
 
 public interface PenseeURL {
-	public static final String URL_AJOUTER_PENSEE = "http://localhost/Service/src/pensee/ajouter/";
-	public static final String URL_LISTE_PENSEES = "http://localhost/Service/src/pensee/liste/";
+	public static final String URL_AJOUTER_PENSEE = "http://localhost:8080/maintenance-service-pensees/Service/src/pensee/ajouter/";
+	public static final String URL_LISTE_PENSEES = "http://localhost:8080/maintenance-service-pensees/Service/src/pensee/liste/";
 }
